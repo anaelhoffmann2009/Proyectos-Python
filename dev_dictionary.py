9898f1c31e8bc0c85e2e1777a60d7fdb3b7a6161
anaelhoffmann2009 <158530923+anaelhoffmann2009@users.noreply.github.com>
2024-02-06 18:21:59 -0300

    dev_dictionary.py
    This is a dictionary app to search for words widely used in the computer world.

---
+++ dev_dictionary.py
@@ -0,0 +1,18 @@
+# Developer dictionary app
+
+dev_dict = {
+    "Linux": "Administrador de hardware que funciona en todas los sistemas operativos basados en él",
+    "Software": "Bloques inmensos de código que ejecutan diversas tareas que le indiquemos",
+    "Python": "Lenguaje de programación sencillo y versátil que el más usado en el mundo de la informática",
+    "Script": "Conjunto de código que cumple una función determinada dentro de un software"
+}
+
+word = str(input("Write a word that you don't understand: "))
+
+def search_in_Dictionary():
+    if word in dev_dict.keys():
+            print(dev_dict[word])
+    else:
+        print("¡Error!: The word is not in dictionary.")
+        
+search_in_Dictionary()
